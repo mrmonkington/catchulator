afbc62c65b00fe0db5ba5848af4cbd40637912f8
Mark Kennedy <mark@microbesng.com>
2024-10-28 17:10:39 +0000

    wip:loading catchments from data files and basic catchment matching

--- catchsim.py
+++ catchsim.py
@@ -1,5 +1,19 @@
 from dataclasses import dataclass
 import csv
+from geopandas.tools import geocode
+from geopandas import read_file, GeoSeries, GeoDataFrame, sjoin
+import yaml
+
+SORT_KEY_MIN=-10e9
+SORT_KEY_MAX=10e9
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
+def d(msg):
+    logging.debug(msg)
+
 
 @dataclass
 class School:
@@ -20,23 +34,39 @@ class School:
     apps_expected: int = 0
     fsm_expected: int = 0
 
-#@dataclass
-#class Child
-#    pref_1: School
-#    pref_2: School
-#    pref_3: School
+@dataclass
+class Application:
+    school: School
+    sort_key: int = 0 
+    def lottery(self):
+        self.sort_key = random.randint(SORT_KEY_MIN, SORT_KEY_MAX)
+
+
+@dataclass
+class Child:
+    pref_1: School
+    pref_2: School
+    pref_3: School
 
 @dataclass
 class Catchment:
+    slug: str
     name: str
     # total of all schools in catchment
-    pan: int
+    pan: int = 0
     # how many children live in catchment
-    apps_actual: int
+    apps_actual: int = 0
     # how many children actually qualify for FSM in this area 
-    fsm_actual: int
+    fsm_actual: int = 0
     # geometry
-    # geo: 
+
+@dataclass
+class Option:
+    slug: str
+    name: str
+    catchments: dict[Catchment]
+    geo: GeoDataFrame 
+
 
 @dataclass
 class LA:
@@ -47,11 +77,11 @@ class LA:
 
 
 def read_school_data(filename):
-    schools = []
+    schools = {}
     with open(filename, newline='') as file_obj:
         reader_object = csv.DictReader(file_obj)
         for row in reader_object:
-            school = School(
+            schools[row['school_id']] = School(
                 slug=row['school_id'],
                 name=row['school_name'],
                 pan=int(row['pan_2024']),
@@ -66,12 +96,51 @@ def read_school_data(filename):
                 third_prefs_accepted=float(row['proportion_1stprefs_v_1stprefoffers']),
                 fsm_eligible_percent=float(row['FSM_eligible_percent'])
             )
-            schools.append(school)
     return schools
+
+def geocode_postcode(postcode) -> GeoDataFrame:
+    postcode_locations = geocode("BN1 1AA, UK", provider="Nominatim", domain="localhost:8080", scheme="http")
+    location = postcode_locations.get_geometry(0)
+    location_gdf = GeoDataFrame({'geometry': location}, crs="EPSG:4326")
+    return location_gdf
+
+def load_options() -> dict[str,Catchment]:
+    options = {}
+    with open("catchments/options.yaml", newline='') as file_obj:
+        options_yml: list = yaml.safe_load(file_obj)
+        for opt_yml in options_yml:
+            catchments: dict[str,Catchment] = {}
+            for cmt_yml in opt_yml["catchments"]:
+                catchments[cmt_yml["slug"]] = Catchment(
+                    **cmt_yml
+                )
+            options[opt_yml["slug"]] = Option( 
+                slug = opt_yml["slug"],
+                name = opt_yml["name"],
+                catchments = catchments,
+                geo = read_file(f"catchments/{opt_yml['geofile']}")
+            )
+    return options
+
+def find_catchment(postcode, option):
+    location_gdf = geocode_postcode(postcode)
+    joined_gdf = sjoin(option.geo, location_gdf, how='inner', predicate='intersects')
+
+    if not joined_gdf.empty:
+        overlapping_feature = joined_gdf.iloc[0]
+        d(f"found catchment {overlapping_feature['catchment']} for postcode {postcode}")
+        return overlapping_feature['catchment']
+    else:
+        d("Point doesn't intersect with any polygon")
+        return False
 
 schools_data = read_school_data('secondary_admissions_actuals_2425.csv')
 
-for school in schools_data:
-    print(school.name, school.pan, school.total_places, school.offers_made, school.offers_accepted)
+for school_id, school in schools_data.items():
+    d((school.name, school.pan, school.total_places, school.offers_made, school.offers_accepted))
+    #print(school)
 
-def 
+options = load_options()
+d("Loaded options")
+
+print(find_catchment("BN1 1AA", options["B"]))
